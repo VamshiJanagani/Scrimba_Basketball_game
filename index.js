5cd3a933c9a62b9094bc59a64afb20625a95aa73
VamshiJanagani <87365180+VamshiJanagani@users.noreply.github.com>
2022-11-29 12:34:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-//let score1=document.getElementById("score").textContent
-//let score2=document.getElementById("guestscore").textContent
+let score1=document.getElementById("score")
+let score2=document.getElementById("guestscore")
 let homesc=12
 let guestsc=5
 function plus1(){
     homesc+=1;
-    document.getElementById("score").innerText=homesc;
+    score1.innerText=homesc;
 }
 function plus2(){
     homesc+=2
-    document.getElementById("score").innerText=homesc;
+    score1.innerText=homesc;
 }
 function plus3(){
     homesc+=3
-    document.getElementById("score").innerText=homesc;
+    score1.innerText=homesc;
 }
 function guestplus1(){
     guestsc+=1
-    document.getElementById("guestscore").innerText=guestsc;
+    score2.innerText=guestsc;
 }
 function guestplus2(){
     guestsc+=2
-    document.getElementById("guestscore").innerText=guestsc;
+    score2.innerText=guestsc;
 }
 function guestplus3(){
     guestsc+=3
-    document.getElementById("guestscore").innerText=guestsc;
+    score2.innerText=guestsc;
 }
